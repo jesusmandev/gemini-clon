729acd93c8c1c90c9db9b15eadf8d84f758c8463
jesusmandev <jesusmanuelserpa23@gmail.com>
2026-04-02 18:38:44 -0500

    Updated program

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? '/gemini-clon/' : '/',
+  base: '/gemini-clon/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
